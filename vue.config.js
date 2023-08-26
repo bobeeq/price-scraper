4f9db91589f3c512593acdc9f905dbcf20c2fec5
bobeeq <bobeeq@ubuntu-server>
2023-08-26 12:39:30 +0200

    build

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/price-scraper/dist'
+  publicPath: '/price-scraper/dist/'
 })
