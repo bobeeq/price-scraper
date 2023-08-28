252a7c4eb51d0334dee98f44d9c1dee00ea03228
bobeeq <bobeeq@ubuntu-server>
2023-08-28 20:05:52 +0200

    auto-deploy

--- src/components/Prices.vue
+++ src/components/Prices.vue
@@ -16,5 +16,28 @@ export default {
         <li v-for="ean of eanStore.eans" :key="ean.code">
             {{ ean.code }}: {{ ean.prices }}
         </li>
+        <div class="gandalf">
+            <div>GANDALF DO EXCELA:</div>
+            <div>
+                <table>
+                    <tr>
+                        <td>ean</td>
+                        <td>zwykła</td>
+                        <td>google</td>
+                    </tr>
+                    <tr v-for="ean of eanStore.eans" :key="ean.code">
+                        <td>{{ ean.code }}</td><td>{{ ean.prices?.gandalf?.default }}</td><td>{{ ean.prices?.gandalf?.google }}</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
     </ul>
 </template>
+
+<style scoped>
+.gandalf {
+    background-color: tomato;
+    margin: 30px;
+    width: 500px;
+}
+</style>
