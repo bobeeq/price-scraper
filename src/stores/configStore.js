4aa52b5a422e1d78e6ad4b9cf22167d0938239dd
bobeeq <bobeeq@ubuntu-server>
2023-08-29 18:54:17 +0200

    auto-deploy

--- src/stores/configStore.js
+++ src/stores/configStore.js
@@ -54,60 +54,59 @@ export const useConfigStore = defineStore('configStore', {
                     },
                     searchURL: ``,
                 },
-                bonito: {
-                    name: 'bonito',
-                    delays: {
-                        min: 10,
-                        max: 15
-                    },
-                    download: {
-                        default: {
-                            value: false,
-                            // async strategy(data) {
-                            //     const {ean, thread} = data;
-                            //     const res = await fetch(
-                            //         `https://classic.bonito.pl/szukaj/${ean.code}/1,0,0/0`,
-                            //         {
-                            //             method: 'GET',
-                            //             credentials: 'include',
-                            //             mode: 'cors',
-                            //             cache: 'no-cache',
-                            //             headers: {
-                            //                 'Host': 'classic.bonito.pl',
-                            //                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0',
-                            //                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-                            //                 'Accept-Language': 'pl,en-US;q=0.7,en;q=0.3',
-                            //                 'Accept-Encoding': 'gzip, deflate, br',
-                            //                 'DNT': '1',
-                            //                 'Upgrade-Insecure-Requests': '1',
-                            //                 'Connection': 'keep-alive',
-                            //                 'Sec-Fetch-Dest': 'document',
-                            //                 'Sec-Fetch-Mode': 'navigate',
-                            //                 'Sec-Fetch-Site': 'none',
-                            //                 'Sec-Fetch-User': '?1',
-                            //                 'Sec-GPC': '1'
-                            //             }
-                            //         }
-                            //     );
-                            //     const text = await res.text();
-                            //     const dom = (new DOMParser()).parseFromString(text, 'text/html');
+                // bonito: {
+                //     name: 'bonito',
+                //     delays: {
+                //         min: 10,
+                //         max: 15
+                //     },
+                //     download: {
+                //         default: {
+                //             value: false,
+                //             async strategy(data) {
+                //                 const {ean, thread} = data;
+                //                 const res = await fetch(
+                //                     `https://classic.bonito.pl/szukaj/${ean.code}/1,0,0/0`,
+                //                     {
+                //                         method: 'GET',
+                //                         credentials: 'include',
+                //                         mode: 'cors',
+                //                         headers: {
+                //                             'Host': 'classic.bonito.pl',
+                //                             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0',
+                //                             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+                //                             'Accept-Language': 'pl,en-US;q=0.7,en;q=0.3',
+                //                             'Accept-Encoding': 'gzip, deflate, br',
+                //                             'DNT': '1',
+                //                             'Upgrade-Insecure-Requests': '1',
+                //                             'Connection': 'keep-alive',
+                //                             'Sec-Fetch-Dest': 'document',
+                //                             'Sec-Fetch-Mode': 'navigate',
+                //                             'Sec-Fetch-Site': 'none',
+                //                             'Sec-Fetch-User': '?1',
+                //                             'Sec-GPC': '1'
+                //                         }
+                //                     }
+                //                 );
+                //                 const text = await res.text();
+                //                 const dom = (new DOMParser()).parseFromString(text, 'text/html');
 
-                            //     const price = dom.querySelector('font[color="black"][style="font-size: 12pt;"] > b')?.textContent.trim().replaceAll(/[^\d,]/g, '');
+                //                 const price = dom.querySelector('font[color="black"][style="font-size: 12pt;"] > b')?.textContent.trim().replaceAll(/[^\d,]/g, '');
 
-                            //     const urlToProduct = dom.querySelector('ul.internal-list .info-box a:first-child')?.href;
-                            //     useConfigStore().bookstores['gandalf'].productPages[ean.code] = urlToProduct;
-                            //     if(!ean.prices[thread.name]) {
-                            //         ean.prices[thread.name] = {};
-                            //     }
-                            //     ean.prices[thread.name].default = price;
-                            // }
-                        },
-                        google: {
-                            value: false,
-                        }
-                    },
-                    searchURL: ``,
-                },
+                //                 const urlToProduct = dom.querySelector('ul.internal-list .info-box a:first-child')?.href;
+                //                 useConfigStore().bookstores['gandalf'].productPages[ean.code] = urlToProduct;
+                //                 if(!ean.prices[thread.name]) {
+                //                     ean.prices[thread.name] = {};
+                //                 }
+                //                 ean.prices[thread.name].default = price;
+                //             }
+                //         },
+                //         google: {
+                //             value: false,
+                //         }
+                //     },
+                //     searchURL: ``,
+                // },
                 tantis: {
                     name: 'tantis',
                     delays: {
@@ -136,37 +135,37 @@ export const useConfigStore = defineStore('configStore', {
                     },
                     searchURL: ``,
                 },
-                'świat_książki': {
-                    name: 'świat_książki',
-                    delays: {
-                        min: 2,
-                        max: 4
-                    },
-                    download: {
-                        default: {
-                            value: false,
-                            // async strategy(data) {
-                            //     const {ean, thread} = data;
+                // 'świat_książki': {
+                //     name: 'świat_książki',
+                //     delays: {
+                //         min: 2,
+                //         max: 4
+                //     },
+                //     download: {
+                //         default: {
+                //             value: false,
+                //             async strategy(data) {
+                //                 const {ean, thread} = data;
 
-                            //     const res = await fetch(`https://www.swiatksiazki.pl/search/${ean.code}`, {
-                            //         mode: 'cors',
-                            //         credentials: 'include',
-                            //         headers: {'User-Agent': 'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Chrome/W.X.Y.Z Safari/537.36'}
-                            //     })
-                            //     const text = await res.text();
-                            //     const dom = (new DOMParser()).parseFromString(text, 'text/html');
+                //                 const res = await fetch(`https://www.swiatksiazki.pl/search/${ean.code}`, {
+                //                     mode: 'cors',
+                //                     credentials: 'include',
+                //                     headers: {'User-Agent': 'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Chrome/W.X.Y.Z Safari/537.36'}
+                //                 })
+                //                 const text = await res.text();
+                //                 const dom = (new DOMParser()).parseFromString(text, 'text/html');
 
-                            //     const price = dom.querySelector('ul.ProductListPage .ProductPrice-PriceValue')?.textContent.replaceAll(/[^\d,]/, '') ?? '-';
+                //                 const price = dom.querySelector('ul.ProductListPage .ProductPrice-PriceValue')?.textContent.replaceAll(/[^\d,]/, '') ?? '-';
 
-                            //     ean.prices[thread.name].default = price;
-                            // }
-                        },
-                        google: {
-                            value: false,
-                        }
-                    },
-                    searchURL: ``,
-                },
+                //                 ean.prices[thread.name].default = price;
+                //             }
+                //         },
+                //         google: {
+                //             value: false,
+                //         }
+                //     },
+                //     searchURL: ``,
+                // },
                 empik_lc: {
                     name: 'empik_lc',
                     delays: {
